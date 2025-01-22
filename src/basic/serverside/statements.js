2796dc444dcc27bc33415ed44fb26414e4f89e77
allecosta <alle_costa10@outlook.com>
2025-01-22 01:49:29 -0300

    feat: added statements and syntax
    Signed-off-by: allecosta <alle_costa10@outlook.com>

---
+++ src/basic/serverside/statements.js
@@ -0,0 +1,17 @@
+let x, y, z;
+x = 4;
+y = 10;
+z = x + y ;
+console.log(z);
+
+let person1 = "José Maria";
+let person2 = "Rosa Maria";
+console.log("O sr " + person1 + " e a sra " + person2 + ".");
+
+myFunc();
+
+function myFunc() {
+    console.log("Olá MUndo JS!");
+}
+
+
---
+++ src/basic/serverside/syntax.js
@@ -0,0 +1,24 @@
+// Declarando variáveis
+let a;
+let b;
+
+// Inicializando variáveis
+a = 4;
+b = 10;
+
+let x = a * b;
+console.log("Resultado: " + x);
+
+let y = (a + b) * 10;
+let z = a - 25;
+console.log("Resultado: " + y);
+console.log("Resultado: " + z);
+
+var c, d;
+c = 4 + 10;
+d = c / 2;
+console.log("Resultado: " + d);
+
+var firstName = "Rosa";
+var lastName = "Maria";
+console.log("Nome: " + firstName + " " + lastName);
